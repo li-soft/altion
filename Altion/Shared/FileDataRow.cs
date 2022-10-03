7d14a23742b623f86cea44e42e9d5b2ccb28f718
Pawel Linek <pawel_linek@interia.pl>
2022-10-03 21:10:56 +0200

    Update FileDataRow.cs

--- Altion/Shared/FileDataRow.cs
+++ Altion/Shared/FileDataRow.cs
@@ -8,21 +8,11 @@ public class FileDataRow
     public static FileDataRow FromString(string value)
     {
         var split = value.Split(". ");
-        try
-        {
-            return new FileDataRow
+        return new FileDataRow
             {
                 Number = long.Parse(split[0]),
                 Text = split[1]
             };
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            Console.WriteLine(split[0]);
-            throw;
-        }
-        
     }
 
     public ReadOnlyMemory<char> ToFileRow() => $"{Number}. {Text}".AsMemory();
